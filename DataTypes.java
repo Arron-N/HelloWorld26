d2f82c09c82c8b1b469a726d15682f012bec6514
Arron Nguyen <arron.nguyen@mrjaffesclass.com>
2022-09-14 14:21:03 -0700

    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes
+{
+    public static void main(String args[])
+    {
+        int i;
+        float f;
+        f = 2.5f;
+        boolean flag;
+        flag = true;
+        System.out.println("What is 5 divided by 2?");
+        System.out.println(f);
+        System.out.print(flag);
+    }
+    
+}

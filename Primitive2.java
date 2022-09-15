d5d54313961f6473e6a94d23490a9f74acf78f70
Arron Nguyen <arron.nguyen@mrjaffesclass.com>
2022-09-15 14:00:06 -0700

    More practice with Primitives

---
+++ Primitive2.java
@@ -0,0 +1,14 @@
+public class Primitive2
+{
+    public static void main(String args[])
+    {
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        {
+            System.out.println("5/2=?");
+            System.out.println(f);
+            System.out.print(flag);
+        }
+    }
+}
